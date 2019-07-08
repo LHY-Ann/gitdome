4a9a64f7976d60d9bb60cff28f4007bbc16fcece
LHY <lhy136890@163.com>
2019-07-08 10:05:52 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <ul>
+      <li>home1</li>
+      <li>home2</li>
+      <li>home3</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+ 
 }
 </script>
